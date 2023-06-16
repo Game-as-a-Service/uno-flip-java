81995b2d6b8903ba7f10b273530d719b62f7bcd1
ankertim <yyho.swe@gmail.com>
2023-06-16 22:45:09 +0800

    chore: init backend infra

---
+++ backend/uno-flip/src/main/java/tw/waterballsa/gaas/unoflip/UnoFlipApplication.java
@@ -0,0 +1,13 @@
+package tw.waterballsa.gaas.unoflip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoFlipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoFlipApplication.class, args);
+	}
+
+}
---
+++ backend/uno-flip/src/test/java/tw/waterballsa/gaas/unoflip/UnoFlipApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.waterballsa.gaas.unoflip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoFlipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
