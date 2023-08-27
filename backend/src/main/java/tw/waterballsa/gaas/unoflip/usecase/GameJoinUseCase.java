56f59bccfe26899e008730313049a80bea2495c9
ShadowSunCat <ss7j0084@gmail.com>
2023-08-27 22:20:15 +0800

    refactor: move send event into use case
    將發送 sse event 跟產 response 的工作搬到 use case 層，這樣 controller 就不用管 presenter。

--- backend/src/main/java/tw/waterballsa/gaas/unoflip/usecase/GameJoinUseCase.java
+++ backend/src/main/java/tw/waterballsa/gaas/unoflip/usecase/GameJoinUseCase.java
@@ -3,19 +3,29 @@ package tw.waterballsa.gaas.unoflip.usecase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import tw.waterballsa.gaas.unoflip.domain.UnoFlipGame;
+import tw.waterballsa.gaas.unoflip.presenter.GameJoinPresenter;
 import tw.waterballsa.gaas.unoflip.repository.GameRepo;
+import tw.waterballsa.gaas.unoflip.service.SseService;
 import tw.waterballsa.gaas.unoflip.vo.GameJoinResult;
+import tw.waterballsa.gaas.unoflip.vo.JoinResult;
+import tw.waterballsa.gaas.unoflip.vo.Response;
 
 @Service
 @RequiredArgsConstructor
 public class GameJoinUseCase {
     private final GameRepo gameRepo;
+    private final GameJoinPresenter gameJoinPresenter;
+    private final SseService sseService;
 
-    public GameJoinResult join(String playerId, String playerName) {
+    public Response<JoinResult> join(String playerId, String playerName) {
         UnoFlipGame availableGame = gameRepo.getAvailable().orElseGet(() -> new UnoFlipGame(gameRepo.generateTableId()));
         availableGame.join(playerId, playerName);
         gameRepo.save(availableGame);
 
-        return new GameJoinResult(availableGame.getTableId(), availableGame.getPlayerInfoList());
+        GameJoinResult gameJoinResult = new GameJoinResult(availableGame.getTableId(), availableGame.getPlayerInfoList());
+
+        sseService.sendMessage(gameJoinPresenter.broadcastEvent(playerId, gameJoinResult));
+
+        return gameJoinPresenter.response(playerId, gameJoinResult);
     }
 }
--- backend/src/test/java/tw/waterballsa/gaas/unoflip/usecase/GameJoinUseCaseTest.java
+++ backend/src/test/java/tw/waterballsa/gaas/unoflip/usecase/GameJoinUseCaseTest.java
@@ -5,10 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import tw.waterballsa.gaas.unoflip.domain.UnoFlipGame;
-import tw.waterballsa.gaas.unoflip.repository.GameRepo;
-import tw.waterballsa.gaas.unoflip.vo.GameJoinResult;
 import tw.waterballsa.gaas.unoflip.domain.PlayerInfo;
+import tw.waterballsa.gaas.unoflip.domain.UnoFlipGame;
+import tw.waterballsa.gaas.unoflip.event.BroadcastEvent;
+import tw.waterballsa.gaas.unoflip.presenter.GameJoinPresenter;
+import tw.waterballsa.gaas.unoflip.presenter.StatusCode;
+import tw.waterballsa.gaas.unoflip.repository.GameRepo;
+import tw.waterballsa.gaas.unoflip.service.SseService;
+import tw.waterballsa.gaas.unoflip.vo.GameJoinResult;
+import tw.waterballsa.gaas.unoflip.vo.JoinResult;
+import tw.waterballsa.gaas.unoflip.vo.Response;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,45 +27,60 @@ import static org.mockito.Mockito.*;
 class GameJoinUseCaseTest {
     private static final String MAX_PLAYER_ID = "max456";
     private static final String SHADOW_PLAYER_ID = "shadow123";
-    private final String MAX_NAME = "Max";
-    private final String SHADOW_NAME = "Shadow";
 
     @Mock
     private GameRepo gameRepo;
+    @Mock
+    private SseService sseService;
+    @Mock
+    private GameJoinPresenter gameJoinPresenter;
     @Mock
     private UnoFlipGame unoFlipGame;
     @Mock
     private UnoFlipGame unoFlipGame1;
     @Mock
     private UnoFlipGame unoFlipGame2;
-    private GameJoinUseCase sut;
-    private List<PlayerInfo> playerInfoList;
-    private GameJoinResult shadowJoinResult;
-    private GameJoinResult maxJoinResult;
-    private List<PlayerInfo> playerInfoList1;
 
+    private List<PlayerInfo> playerInfoList;
+    private List<PlayerInfo> playerInfoList1;
     private List<PlayerInfo> playerInfoList2;
+    private Response<JoinResult> joinTableResponse;
+    private Response<JoinResult> joinTable1Response;
+    private Response<JoinResult> joinTable2Response;
+    private GameJoinUseCase sut;
+
 
     @BeforeEach
     void setUp() {
-        sut = new GameJoinUseCase(gameRepo);
+        sut = new GameJoinUseCase(gameRepo, gameJoinPresenter, sseService);
     }
 
     @Test
     void should_create_new_game_when_no_available_game() {
         given_no_available_game();
-        given_next_table_id_is_123();
+        given_new_game_table_id_is_123();
 
-        when_join_game_then_table_id_is_123();
+        when_someone_join();
+
+        then_table_id_is_123();
     }
 
     @Test
     void should_save_game() {
         when(gameRepo.getAvailable()).thenReturn(Optional.of(unoFlipGame));
 
-        sut.join(SHADOW_PLAYER_ID, SHADOW_NAME);
+        when_shadow_join();
 
         verify(gameRepo).save(unoFlipGame);
+    }
+
+    @Test
+    void should_send_event() {
+        BroadcastEvent event = given_broadcast_event();
+
+        when_shadow_join();
+
+        verify(sseService).sendMessage(event);
     }
 
     @Test
@@ -69,7 +90,7 @@ class GameJoinUseCaseTest {
         given_max_join_table_2();
 
         when_shadow_join();
-        then_shadow_should_at_position(1);
+        then_shadow_should_at_position_1();
 
         when_max_join();
         then_max_should_at_position(1);
@@ -84,12 +105,16 @@ class GameJoinUseCaseTest {
         given_max_is_the_second_player_of_the_game();
 
         when_shadow_join();
-        then_shadow_should_at_position(1);
+        then_shadow_should_at_position_1();
 
         when_max_join();
         then_max_should_at_position(2);
 
         then_shadow_and_max_should_in_the_same_game();
+    }
+
+    private void when_someone_join() {
+        joinTableResponse = sut.join("SOMEONE", "NO_CARE");
     }
 
     private void init_game() {
@@ -116,60 +141,77 @@ class GameJoinUseCaseTest {
     }
 
     private void given_shadow_is_the_first_player_of_the_game() {
-        playerInfoList.add(new PlayerInfo(SHADOW_PLAYER_ID, SHADOW_NAME, 1));
+        Response<JoinResult> response = given_response(1, 1);
+        when(gameJoinPresenter.response(SHADOW_PLAYER_ID, new GameJoinResult(1, playerInfoList))).thenReturn(response);
     }
 
     private void given_max_is_the_second_player_of_the_game() {
-        playerInfoList.add(new PlayerInfo(MAX_PLAYER_ID, MAX_NAME, 2));
+        Response<JoinResult> response = given_response(1, 2);
+        when(gameJoinPresenter.response(MAX_PLAYER_ID, new GameJoinResult(1, playerInfoList))).thenReturn(response);
     }
 
     private void given_shadow_join_table_1() {
-        playerInfoList1.add(new PlayerInfo(SHADOW_PLAYER_ID, SHADOW_NAME, 1));
-    }
-
-    private void given_next_table_id_is_123() {
-        when(gameRepo.generateTableId()).thenReturn(123);
+        Response<JoinResult> response = given_response(1, 1);
+        when(gameJoinPresenter.response(SHADOW_PLAYER_ID, new GameJoinResult(1, playerInfoList1))).thenReturn(response);
     }
 
     private void given_max_join_table_2() {
-        playerInfoList2.add(new PlayerInfo(MAX_PLAYER_ID, MAX_NAME, 1));
+        Response<JoinResult> response = given_response(2, 1);
+        when(gameJoinPresenter.response(MAX_PLAYER_ID, new GameJoinResult(2, playerInfoList2))).thenReturn(response);
     }
 
-    private void when_join_game_then_table_id_is_123() {
-        assertThat(sut.join(SHADOW_PLAYER_ID, SHADOW_NAME).tableId()).isEqualTo(123);
+    private Response<JoinResult> given_response(int tableId, int position) {
+        JoinResult joinResult = given_join_result(tableId, position);
+        return new Response<>(StatusCode.OK.getCode(), "join successfully", joinResult);
+    }
+
+    private void given_new_game_table_id_is_123() {
+        when(gameRepo.generateTableId()).thenReturn(123);
+
+        JoinResult joinResult = mock(JoinResult.class);
+        when(joinResult.tableId()).thenReturn(123);
+        Response<JoinResult> response = new Response<>(StatusCode.OK.getCode(), "join successfully", joinResult);
+        when(gameJoinPresenter.response(eq("SOMEONE"), isA(GameJoinResult.class))).thenReturn(response);
+    }
+
+    private JoinResult given_join_result(int tableId, int position) {
+        JoinResult joinResult = mock(JoinResult.class);
+        when(joinResult.tableId()).thenReturn(tableId);
+        when(joinResult.position()).thenReturn(position);
+        return joinResult;
+    }
+
+    private BroadcastEvent given_broadcast_event() {
+        BroadcastEvent event = mock(BroadcastEvent.class);
+        when(gameJoinPresenter.broadcastEvent(eq(SHADOW_PLAYER_ID), isA(GameJoinResult.class))).thenReturn(event);
+        return event;
     }
 
     private void when_shadow_join() {
-        shadowJoinResult = sut.join(SHADOW_PLAYER_ID, SHADOW_NAME);
+        joinTable1Response = sut.join(SHADOW_PLAYER_ID, "Shadow");
     }
 
     private void when_max_join() {
-        maxJoinResult = sut.join(MAX_PLAYER_ID, MAX_NAME);
+        joinTable2Response = sut.join(MAX_PLAYER_ID, "Max");
     }
 
-    private void then_shadow_should_at_position(int expectedPosition) {
-        Integer shadowPosition = shadowJoinResult.playerInfoList().stream()
-                .filter(playerInfo -> SHADOW_PLAYER_ID.equals(playerInfo.id()))
-                .map(PlayerInfo::position)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("player %s not int game".formatted(SHADOW_PLAYER_ID)));
-        assertThat(shadowPosition).isEqualTo(expectedPosition);
+    private void then_table_id_is_123() {
+        assertThat(joinTableResponse.payload().tableId()).isEqualTo(123);
     }
 
-    private void then_shadow_and_max_should_in_different_game() {
-        assertThat(shadowJoinResult.tableId()).isNotEqualTo(maxJoinResult.tableId());
-    }
-
-    private void then_shadow_and_max_should_in_the_same_game() {
-        assertThat(shadowJoinResult.tableId()).isEqualTo(maxJoinResult.tableId());
+    private void then_shadow_should_at_position_1() {
+        assertThat(joinTable1Response.payload().position()).isEqualTo(1);
     }
 
     private void then_max_should_at_position(int exceptedPosition) {
-        Integer maxPosition = maxJoinResult.playerInfoList().stream()
-                .filter(playerInfo -> MAX_PLAYER_ID.equals(playerInfo.id()))
-                .map(PlayerInfo::position)
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("player %s not int game".formatted(MAX_PLAYER_ID)));
-        assertThat(maxPosition).isEqualTo(exceptedPosition);
+        assertThat(joinTable2Response.payload().position()).isEqualTo(exceptedPosition);
+    }
+
+    private void then_shadow_and_max_should_in_different_game() {
+        assertThat(joinTable1Response.payload().tableId()).isNotEqualTo(joinTable2Response.payload().tableId());
+    }
+
+    private void then_shadow_and_max_should_in_the_same_game() {
+        assertThat(joinTable1Response.payload().tableId()).isEqualTo(joinTable2Response.payload().tableId());
     }
 }
